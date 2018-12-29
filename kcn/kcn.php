9ce4bbd2958047e09185725e48ff1efc584a002b
Raghavendra <raghav.php@gmail.com>
2018-12-29 18:35:09 +0530

    Kannada Chanakya Neeti - v1.0.1 Wordpress Plugin

---
+++ kcn/kcn.php
@@ -0,0 +1,123 @@
+<?php
+/*
+Plugin Name: Kannada Chankya Neeti
+Plugin URI: https://www.sharadhiinfotech.com/wordpress-plugins-kannada-chankya-neeti/
+Description: This plugin creates a widget which Show a random kannada verse from Chanakya Niti written by Chanakya on your website!. You can add this widget to any widgetized areas, such as sidebars.
+Author: Raghavendra Uppunda
+Version: 1.0.1
+Author URI: https://www.sharadhiinfotech.com/
+*/
+
+/*  This file is part of Kannada Chankya Neeti plugin, developed by Raghavendra Uppunda (email: contact@sharadhiinfotech.com)
+
+	Chanakya Neeti is a collection of aphorisms, said to be selected by Chanakya from the various shastras.
+
+    Kannada Chankya Neeti is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kannada Chankya Neeti is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kannada Chankya Neeti plugin. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function kcn_get_neeti() {
+	$kcnfile = plugins_url( 'kcn.txt' , __FILE__ );
+	$lines = file("$kcnfile"); 
+
+	// Randomly choose a verse
+	return wptexturize( $lines[ mt_rand( 0, count( $lines ) - 1 ) ] );
+}
+
+function kcn_show_neeti() {
+	$kcn = kcn_get_neeti();
+	
+	echo "<p id='kcn' class='kcn'>$kcn</p>";
+}
+
+
+class Kannada_Chanakya_Neeti_Widget extends WP_Widget {
+			
+	function __construct() {
+    	$widget_ops = array(
+			'classname'   => 'kannada_chanakya_neeti', 
+			'description' => __('Show a random kannada verse from Chanakya Niti written by Chanakya on your website!')
+		);
+    	parent::__construct('kannada-chanakya-neeti', __('Kannada Chanakya Neeti'), $widget_ops);
+	}
+	
+	function widget($args, $instance) {
+           
+			extract( $args );
+		
+			$title = apply_filters( 'widget_title', empty($instance['title']) ? ' ಚಾಣಕ್ಯ ನೀತಿ ' : $instance['title'], $instance, $this->id_base);	
+			
+			
+			echo $before_widget;
+			
+			
+			// Widget title
+			
+			echo $before_title;
+			
+			echo $instance["title"];
+			
+			echo $after_title;
+			
+			
+			// Call show_kcn function
+			
+    			kcn_show_neeti();
+
+		echo $after_widget;
+
+	}
+	
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+	     
+        		return $instance;
+	}
+	
+	
+	function form( $instance ) {
+		$title = isset($instance['title']) ? esc_attr($instance['title']) : ' ಚಾಣಕ್ಯ ನೀತಿ ';
+		
+?>
+        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+             
+<?php
+	}
+}
+
+function kcn_register_widgets() {
+	register_widget( 'Kannada_Chanakya_Neeti_Widget' );
+}
+
+add_action( 'widgets_init', 'kcn_register_widgets' );
+
+// Donate link on manage plugin page
+function kcn_pluginspage_links( $links, $file ) {
+
+$plugin = plugin_basename(__FILE__);
+
+// create links
+if ( $file == $plugin ) {
+return array_merge(
+$links,
+array( '<a href="https://www.sharadhiinfotech.com/" target="_blank" title="Contact for Help/Support ">Support</a>',
+ )
+);
+			}
+return $links;
+
+	}
+add_filter( 'plugin_row_meta', 'kcn_pluginspage_links', 10, 2 );
+?>
---
+++ kcn/readme.txt
@@ -0,0 +1,50 @@
+=== Kannada Chanakya Neeti ===
+Contributors: Raghavendra Uppunda
+License: GPLv3
+License URI: http://www.gnu.org/copyleft/gpl.html
+Tags: kannada, unicode, literary, niti, neeti, chanakya, Kauṭilya, ancient, Vishnugupta,  news, random, verse, daily,  widget
+Requires at least: 3.0
+Tested up to: 5.0.2
+Stable tag: 1.0.1
+
+Show a random kannada verse from Chanakya Niti written by Chanakya on your website!
+
+== Description ==
+
+Chanakya Neeti is a collection of aphorisms, said to be selected by Chanakya from the various shastras.
+Its a Random collection of aphorisms from Chanakya Neeti will be displayed.
+
+== Installation ==
+
+1. Upload the `kcn` folder to the `/wp-content/plugins/` directory
+2. Activate the Kannada Chankya Neeti plugin through the 'Plugins' menu in WordPress
+3. Go to Appearance --> Widgets to add the Kannada Chankya Neeti widget to any widgetized areas, such as sidebars.
+
+== Screenshots ==
+
+1. Kannada Chankya Neeti widget showing a random aphorisms
+2. Kannada Chankya Neeti widget showing another random aphorisms
+3. Another Chankya Neeti
+4. Kannada Chankya Neeti in Appearance > Widgets
+5. Kannada Chankya Neeti when added to sidebar. You can change it's title.
+
+== Frequently Asked Questions ==
+
+= How many poems are there? =
+There are actually 1000s of aphorisms in  Chankya Neeti. But this plugin has just over 200 aphorisms. I'll add more aphorisms in the future updates.
+
+= How to style the widget? =
+The poem output wrapped in `<p>` tag with `kcn` ID and class. Use `#kcn` or `.kcn` in your Custom CSS to style it.
+
+= Can I add more poems to this plugin? =
+Yeah, sure! You can add more poems to this plugin. Just open `kcn.txt` and add new aphorisms at the bottom of the file. If you send me the updated file, I'll upload it to wordpress plugin repository.
+
+= How can I contact you? =
+Use my <a href="https://www.sharadhiinfotech.com/contact/">contact form</a> to shoot me an email.
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial public release.
+
